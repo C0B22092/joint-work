9bc7c4a75205c59643f0d311e951081f9c9c67eb
C0B22092 <c0b22092@edu.teu.ac.jp>
2022-06-23 18:01:43 +0900

    third commit

--- janken.py
+++ janken.py
@@ -1,5 +1,8 @@
 import random
 print("0~2の整数を入力せよ[0:グー,1:チョキ,2:パー]")
+cnt_w = 0
+cnt_l = 0
+cnt_d = 0
 for i in range(3):
     you = 100
     while you >= 3:
@@ -7,8 +10,12 @@ for i in range(3):
         cpu = random.randint(0,2)
         print("CPU:",cpu,end = " ")
         if you==cpu:
+            cnt_d += 1
             print("Draw!")
         elif (you+1)%3==cpu:
+            cnt_w += 1
             print("You Win!")
         else:
+            cnt_l += 1
             print("You Lose!")
+print(f"{cnt_w}勝、{cnt_l}敗、{cnt_d}引き分け")
